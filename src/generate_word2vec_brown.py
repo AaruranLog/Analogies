856af19f788b1f08929d68e500e8006b730cda46
Elamurugaiyan, Aaruran <Aaruran.Elamurugaiyan@capitalone.com>
2017-12-19 16:06:06 -0500

    refactored script

--- src/generate_word2vec_brown.py
+++ src/generate_word2vec_brown.py
@@ -1,35 +1,9 @@
-
-# coding: utf-8
-
-# In[3]:
-
 import gensim.models.word2vec
 from nltk.corpus import brown
-
-
-# In[6]:
-
-len(brown.words())
-
-
-# In[9]:
-
-brown.categories()
-
-
-# In[10]:
-
-brown.words(categories="humor")
-
-
-# In[11]:
-
-brown.sents(categories="humor")
-
-
-# In[63]:
+import datetime
 
 class my_sentences():
+    """Implements iterator protocol to yield sentences from brown corpus"""
     def __init__(self):
         self.permitted_categories = ["humor", "lore", "adventure"]
         self.corpus = brown
@@ -48,42 +22,23 @@ class my_sentences():
             except IndexError:
                 continue
         raise StopIteration
-    
 
+def main():
+    """
+        Entry point
+    """
+    model = gensim.models.word2vec.Word2Vec(sentences = my_sentences())
+    todays_date = datetime.date.today().strftime("%Y%m%d")
+    model_name = "brown-" + todays_date + ".model"
+    model.save("/Users/fxf231/Documents/git-repos/Analogies/notebooks/" + model_name)
 
-# In[68]:
+    print("=" * 20)
+    print("Tests")
+    print("=" * 20)
+    print("expected: girl")
+    print("Actual: ")
+    print(model.doesnt_match("man boy girl".split(" ")))
 
-model = gensim.models.word2vec.Word2Vec(sentences = my_sentences())
-
-
-# In[87]:
-
-import datetime
-todays_date = datetime.date.today().strftime("%Y%m%d")
-model_name = "brown-" + todays_date + ".model"
-
-
-# In[71]:
-
-model.save("/Users/fxf231/Documents/git-repos/Analogies/notebooks/brown")
-
-
-# In[73]:
-
-model.build_vocab(my_sentences())
-
-
-# In[85]:
-
-model.doesnt_match("man boy girl".split(" "))
-
-
-# In[86]:
-
-model.doesnt_match("balance account interest fish".split(" "))
-
-
-# In[ ]:
-
-
-
+    print("expected: fish")
+    print("Actual: ")
+    model.doesnt_match("balance account interest fish".split(" "))
