cae6933e6cac821ffe4c86934253c54c4c66e913
Elamurugaiyan, Aaruran <Aaruran.Elamurugaiyan@capitalone.com>
2017-12-19 16:16:28 -0500

    refactored, added more print statements for usability

--- src/generate_word2vec_brown.py
+++ src/generate_word2vec_brown.py
@@ -1,25 +1,30 @@
+"""
+    Generates a word2vec model trained on categories from nltk's Brown corpus
+"""
+import datetime
 import gensim.models.word2vec
 from nltk.corpus import brown
-import datetime
 
-class my_sentences():
+class CustomCorpus():
     """Implements iterator protocol to yield sentences from brown corpus"""
-    def __init__(self):
-        self.permitted_categories = ["humor", "lore", "adventure"]
+    def __init__(self, categories=["humor", "lore", "adventure",
+                                   "fiction", "learned", "news", "reviews"]):
+        self.permitted_categories = categories
+        self.__usable_categories = self.permitted_categories
         self.corpus = brown
         self.index = 0
-        
+
     def __iter__(self):
         return self
-    
+
     def __next__(self):
-        
-        for c in self.permitted_categories:
+        for category in self.__usable_categories:
             try:
-                result = brown.sents(categories=c)[self.index]
+                result = brown.sents(categories=category)[self.index]
                 self.index += 1
                 return result
             except IndexError:
+                self.__usable_categories.remove(category)
                 continue
         raise StopIteration
 
@@ -27,10 +32,11 @@ def main():
     """
         Entry point
     """
-    model = gensim.models.word2vec.Word2Vec(sentences = my_sentences())
+    model = gensim.models.word2vec.Word2Vec(sentences=CustomCorpus())
     todays_date = datetime.date.today().strftime("%Y%m%d")
     model_name = "brown-" + todays_date + ".model"
     model.save("/Users/fxf231/Documents/git-repos/Analogies/notebooks/" + model_name)
+    print("Model saved")
 
     print("=" * 20)
     print("Tests")
@@ -42,3 +48,7 @@ def main():
     print("expected: fish")
     print("Actual: ")
     model.doesnt_match("balance account interest fish".split(" "))
+
+
+if __name__ == "__main__":
+    main()
