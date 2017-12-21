e375d88ada02213513f40651cdd4473de7d3651b
Elamurugaiyan, Aaruran <Aaruran.Elamurugaiyan@capitalone.com>
2017-12-21 11:18:22 -0500

    config for app

--- analogies/__init__.py
+++ analogies/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from analogies.config.config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from analogies import routes
 from analogies import backend
