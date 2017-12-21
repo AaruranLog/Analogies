586125fc4005ab8cdf4b2947f687e6543aba7989
Elamurugaiyan, Aaruran <Aaruran.Elamurugaiyan@capitalone.com>
2017-12-21 11:17:49 -0500

    new setup and templates

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='analogies',
-    version='0.1',
+    version='0.1.2.dev',
     description="Analogy solver, with Flask front-end",
     author="Aaruran Elamurugaiyan",
     url="https://github.com/AaruranE/Analogies",
-    packages=['app/backend']
+    packages=find_packages(".", exclude=["tests"]),
+    include_package_data=True,
+    license="MIT"
 )
